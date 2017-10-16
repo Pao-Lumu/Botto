0bbe9733608b264196e0476d170d377edeb34108
evan <evanjohnman@github.com>
2017-10-16 18:58:30 -0500

    nope

--- modules/birthday.py
+++ modules/birthday.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import utilities
-from .utils.botto_sql import AccountSQL, GuildSQL
+from utils.botto_sql import AccountSQL, GuildSQL
+from utils.announcement import Announcement
 import datetime
 import discord
 
@@ -10,6 +11,32 @@ class Birthday:
         self.bot = bot
         self.db = AccountSQL()
         self.gdb = GuildSQL()
+        # await _bday_loop()
+
+    @commands.command()
+    async def _bday_loop(self):
+        guilds = gdb.get_all_guild_birthday_channels()
+        all_channels = []
+        x = datetime.datetime.now().date()
+        y, m, d = (x.year, x.month, x.day)
+        for channel in guild:
+            bdchannel = discord.Object(id=guild[0], time=(0,0,0,0))
+            bdays = self.db.get_users_with_birthday(d,m)
+            e = discord.Embed()
+            e.set_author(name="Birthdays today:")
+            if bdays:
+                for user in bdays:
+                    r = discord.utils.get(bdchannel.guild.members, id=user[0])
+                    if r != None:
+                        e.add_field(name=r.name, value='Age: '+ str(y-int(user[1])))
+            else:
+                e.description = "Looks like no one's having a birthday today."
+            all_channels.append((Announcement(self.bot, bdchannel), e))
+        # while True:
+        if True:
+            for ann, em in all_channels:
+                await ann.say_list(em)
+            await asyncio.sleep(10)
 
     @commands.command(pass_context=True, aliases=['setbday', 'sbd'])
     async def setbirthday(self, ctx):
@@ -25,14 +52,14 @@ class Birthday:
             e = await utilities.success_embed('Set birthday for {} to {}-{}-{}!'.format(ctx.message.author.name, year, month, day))
             await self.bot.say(embed=e)
         else:
-            e = await utilities.error_embed('Sorry, that's the wrong format. Be sure to use the format: YYYY-MM-DD')
+            e = await utilities.error_embed("Sorry, that's the wrong format. Be sure to use the format: YYYY-MM-DD")
 
-    @commands.command(pass_context=True, aliases=['bdt', 'birthdaytoday', ''bdaystoday', 'bdaytoday'])
+    @commands.command(pass_context=True, aliases=['bdt', 'birthdaytoday', 'bdaystoday', 'bdaytoday'])
     async def birthdaystoday(self, ctx):
         """Displays people who have birthdays today."""
         x = datetime.datetime.now().date()
         guild = ctx.message.server
-        y,m,d = (x.year, x.month, x.day)
+        y, m, d = (x.year, x.month, x.day)
         bdays = self.db.get_users_with_birthday(d,m)
         e = discord.Embed()
         e.set_author(name="Birthday's today:")
@@ -63,18 +90,28 @@ class Birthday:
         """Shows a server's birthday announcement channel and the user's currently set birthday."""
         cid = self.gdb.get_guild_birthday_channel(ctx.message.server.id)[0]
         channel = discord.utils.get(ctx.message.server.channels, id=cid)
-        if ctx.message.mentions >= 1:
+        if len(ctx.message.mentions) >= 1:
             user = ctx.message.mentions[0].id
         else:
             user = ctx.message.author.id
         uid = self.db.get_user_birthday(ctx.message.author.id)
         if uid != None:
-            birthday = '{}-{}-{}'.format(y[2], y[1], y[0])
+            birthday = '{}-{}-{}'.format(uid[2], uid[1], uid[0])
         else:
             birthday = 'Not Set'
         e = await utilities.info_embed('Birthday channel: {}\nUser Birthday: {}'.format(channel.name, birthday))
         await self.bot.say(embed=e)
+        x = Announcement(self.bot, ctx.message.channel, time=(0,0,1,0))
+        await x.wait_for_time(content="Smoq on the watani")
 
+    @commands.command(pass_context=True, hidden=True)
+    async def ggez(self, ctx):
+        x = Announcement(self.bot, ctx.message.channel, time=(0,0,0,3))
+        ppap = ctx.message.channel.id
+        await x.wait_for_time(content=ppap)
+        shan = discord.Object(id=ppap)
+        y = Announcement(self.bot, shan, time=(0,0,0,1))
+        await y.wait_for_time(content='hahg')
 
     async def extract_cmd_text(self, ctx, spaces=-1, chr=' ', index=1):
         if spaces == -1:
@@ -87,4 +124,3 @@ def setup(bot):
     bot.add_cog(Birthday(bot))
     print('Loaded module birthday')
 
-# def teardown()
---
+++ utils/botto_sql.py
@@ -0,0 +1,158 @@
+import sqlite3
+import os.path
+
+class BaseSQL:
+    def __init__(self, filename='botto.db'):
+        if not os.path.exists(filename):
+            self.db = sqlite3.connect(filename)
+            self.cursor = self.db.cursor()
+            self.cursor.execute('''CREATE TABLE pasta(pasta_tag text, pasta_text text, creator_id text, creation_date text, uses integer, likes integer, dislikes integer)''')
+            self.cursor.execute('''CREATE TABLE account(user_id text, bday_day text, bday_month text, bday_year text)''')
+            self.cursor.execute('''CREATE TABLE guild(guild_id text, bday_channel text)''')
+        else:
+            self.db = sqlite3.connect(filename)
+            self.cursor = self.db.cursor()
+
+    def close(self):
+        self.db.commit()
+        self.db.close()
+
+    def close_no_save(self):
+        self.db.close()
+
+
+
+
+
+
+# accounts: (user_id text, bday_day int, bday_month int, bday_year int)
+class AccountSQL(BaseSQL):
+    def add(self, user_id, day=None, month=None, year=None):
+        if not self.exists(user_id):
+            self.cursor.execute("INSERT INTO account VALUES(?,?,?,?)", (user_id, day, month, year,))
+        else:
+            self.cursor.execute("UPDATE account SET bday_day=?, bday_month=?, bday_year=? WHERE user_id=?", (day, month, year, user_id))
+        self.db.commit()
+
+    def exists(self, user_id):
+        self.cursor.execute("SELECT * FROM account WHERE user_id=?", (user_id,))
+        if self.cursor.fetchone():
+            return True
+        else:
+            return False
+
+    def set_user_birthday(self, user_id, day, month, year):
+        self.add(user_id, day, month, year)
+
+    def get_user_birthday(self, user_id):
+        if self.exists(user_id):
+            self.cursor.execute("SELECT bday_day, bday_month, bday_year FROM account WHERE user_id=?", (user_id,))
+            return self.cursor.fetchone()
+        else:
+            return None
+
+    def get_users_with_birthday(self, day, month):
+        self.cursor.execute("SELECT user_id, bday_year FROM account WHERE bday_day=? AND bday_month=?", (day, month,))
+        return self.cursor.fetchall()
+
+    def get_user_data(self, user_id, field='*'):
+        return
+
+
+
+
+
+
+# (guild_id text, bday_channel text)
+class GuildSQL(BaseSQL):
+    def add(self, guild_id, channel_id=None):
+        if not self.exists(guild_id):
+            self.cursor.execute("INSERT INTO guild VALUES(?,?)", (guild_id, channel_id,))
+        else:
+            self.cursor.execute("UPDATE guild SET bday_channel=? WHERE guild_id=?", (channel_id, guild_id))
+        self.db.commit()
+
+    def exists(self, guild_id):
+        self.cursor.execute("SELECT * FROM guild WHERE guild_id=?", (guild_id,))
+        if self.cursor.fetchone():
+            return True
+        else:
+            return False
+
+    def set_guild_birthday_channel(self, guild_id, channel_id):
+        self.add(guild_id, channel_id)
+        return True
+
+    def get_guild_birthday_channel(self, guild_id):
+        self.cursor.execute("""SELECT bday_channel FROM guild WHERE guild_id=?""", (guild_id,))
+        return self.cursor.fetchone()
+
+    def get_guild_data(self, guild_id, field='*'):
+        self.cursor.execute("""SELECT * FROM guild WHERE guild_id=?""", (guild_id,))
+        return self.cursor.fetchone()
+
+    def get_all_guild_birthday_channels(self):
+        self.cursor.execute("""SELECT bday_channel FROM guild""")
+        return self.cursor.fetchall()
+
+
+
+
+# (pasta_tag text, pasta_text text, creator_id text, creation_date text, uses integer, likes integer, dislikes integer)
+class PastaSQL(BaseSQL):
+    def add(self, args: tuple):
+        self.cursor.execute('''INSERT INTO pasta VALUES (?,?,?,?,?,?,?)''', args)
+        self.db.commit()
+
+    def exists(self, pasta_tag):
+        self.cursor.execute("""SELECT * FROM pasta WHERE pasta_tag=?""", (pasta_tag,))
+        if self.cursor.fetchone() is None:
+            return False
+        return True
+
+    def pasta_owned(self, pasta_tag, user_id):
+        self.cursor.execute("""SELECT * FROM pasta WHERE pasta_tag=? AND creator_id=?""", (pasta_tag, user_id))
+        if self.cursor.fetchone() is None:
+            return False
+        return True
+
+    def get(self, pasta_tag):
+        self.cursor.execute("""SELECT pasta_text,uses FROM pasta WHERE pasta_tag=?""", (pasta_tag,))
+        return self.cursor.fetchone()
+
+    def get_owned(self, user_id):
+        self.cursor.execute("""SELECT pasta_text,uses FROM pasta WHERE creator_id=?""", (user_id,))
+        return self.cursor.fetchall()
+
+    def get_info(self, pasta_tag):
+        self.cursor.execute("""SELECT * FROM pasta WHERE pasta_tag=?""", (pasta_tag,))
+        return self.cursor.fetchone()
+
+    def delete(self, pasta_tag):
+        self.cursor.execute("""DELETE FROM pasta WHERE pasta_tag=?""", (pasta_tag,))
+        self.db.commit()
+
+    def update_text(self, pasta_tag, pasta_text):
+        self.cursor.execute('''UPDATE pasta SET pasta_text=? WHERE pasta_tag=?''', (pasta_text, pasta_tag))
+        self.db.commit()
+
+    def update_uses(self, uses, pasta_tag):
+        self.cursor.execute('''UPDATE pasta SET uses=? WHERE pasta_tag=?''', (uses, pasta_tag))
+        self.db.commit()
+
+    def popular(self):
+        self.cursor.execute('''SELECT pasta_tag,uses FROM pasta ORDER BY uses DESC LIMIT 12''')
+        return self.cursor.fetchall()
+
+    def top(self):
+        self.cursor.execute('''SELECT pasta_tag,net_vote FROM pasta ORDER BY net_vote DESC LIMIT 12''')
+        return
+
+    def vote_up(self, pasta_tag):
+        self.cursor.execute('''UPDATE pasta SET dislikes=likes+1, net_vote=net_vote+1''')
+        return
+
+    def vote_down(self, pasta_tag):
+        self.cursor.execute('''UPDATE pasta SET dislikes=dislikes+1, net_vote=net_vote-1''')
+        return
+
