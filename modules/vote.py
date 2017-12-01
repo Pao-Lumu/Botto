a01c3c3762e7e6c4eb8826b7cb03c0a5d74f3419
Evan Johnston <8527287+TheGrammarJew@users.noreply.github.com>
2017-12-01 16:34:04 -0600

    Update vote.py

--- modules/vote.py
+++ modules/vote.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import utils.utilities as utilities
 
 
 class Vote:
@@ -6,7 +7,7 @@ class Vote:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=['poll', 'yesorno'])
     async def vote(self, ctx):
         """
         <question>
@@ -14,11 +15,11 @@ class Vote:
         :return:
         """
         cmd = await self.extract_cmd_text(ctx, 1)
-        actual_vote = await self.bot.say(cmd[0])
+        actual_vote = await self.bot.say("Poll: " + str(cmd[0]))
         for emoji in ('\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}'):
             await self.bot.add_reaction(actual_vote, emoji)
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=['pollopt'])
     async def voteopt(self, ctx):
         """
         <question>|<option1>|<option2>|...|<option 9>
@@ -27,7 +28,10 @@ class Vote:
         """
         cmd = await self.extract_cmd_text(ctx, 9, chr='|', index=0)
         cmd[0] = cmd[0].split(' ', 1)[1]
-        if len(cmd) <= 10:
+        if len(cmd) > 10:
+            e = await utilities.error_embed("Sorry, I can have up to 9 options on a poll.")
+            await self.bot.say(embed=e)
+        elif len(cmd) <= 10:
             num2word = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣']
             text = 'Question: ' + cmd[0] + '\n\n'
             num = 0
