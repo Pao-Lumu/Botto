dbd304fab3b64d32cc53ed9cf5df4fb8643660cc
Evan Johnston <evan@pfg.dom>
2019-04-11 19:14:27 -0500

    Comrade.py should be fixed(tm)

--- modules/comrade.py
+++ modules/comrade.py
@@ -2,6 +2,7 @@ import asyncio
 # from utils import checks
 import datetime
 import random
+import re
 
 import discord
 from discord.ext import commands
@@ -26,93 +27,104 @@ class Comrade(commands.Cog):
         if msg.clean_content:
             if msg.clean_content[0] == '>':
                 return
-        breeki = msg.clean_content.split(" ")
         cyka, blyat = False, False
-        vodka = []
         if self.bot.cooldown_blyat + 3600 < datetime.datetime.now().timestamp():
             blyat = True
 
-        comrades = {"I": "We", "i": "we", "I'm": "We're", "i'm": "we're", "I'll": "We'll", "i'll": "we'll",
-                    "I'd": "We'd",
-                    "i'd": "we'd", "I've": "We've", "i've": "we've", "my": "our", "mine": "ours", "My": "Our",
-                    "Mine": "Ours", "am": "are", "Am": "Are", "Me": "Us", "me": "us"}
-        for cheeki in breeki:
-            if cheeki in comrades.keys():
-                vodka.append("{}".format(comrades[cheeki]))
+        comrades = {"i": "we",
+                    "me": "us",
+                    "am": "are",
+                    "was": "were",
+                    "'m": " are",
+                    "'ll": " will",
+                    "'d": " would",
+                    "'ve": " have",
+                    "my": "our",
+                    "mine": "ours",
+                    'myself': 'ourselves'}
+        ptn = re.compile("""(i|me|myself|mine|my|am|was)(\\.|\\?|!|,|:|;|"|-|'m|'|\n|\\s)""", flags=re.IGNORECASE)
+        raw = re.split(ptn, msg.clean_content)
+
+        for cheeki, breeki in enumerate(raw):
+            if breeki.casefold() in comrades.keys():
+                if breeki.islower():
+                    raw[cheeki] = comrades[breeki.casefold()].lower()
+                elif breeki.istitle():
+                    raw[cheeki] = comrades[breeki.casefold()].capitalize()
+                elif breeki.isupper():
+                    raw[cheeki] = comrades[breeki.casefold()].upper()
+                else:
+                    raw[cheeki] = comrades[breeki.casefold()]
                 cyka = True
-            else:
-                vodka.append(cheeki)
+
         if cyka:
-            await msg.channel.send("*" + " ".join(vodka) + "\n*Soviet Anthem Plays*")
+            await msg.channel.send("*" + " ".join(raw) + "\n*Soviet Anthem Plays*")
             self.bot.cooldown_cyka = datetime.datetime.now().timestamp()
         if msg.author.voice_channel and not msg.author.is_afk and cyka and blyat:
             self.bot.cooldown_blyat = datetime.datetime.now().timestamp()
-            self.bot.v = await msg.author.voice_channel.connect()
-            player = self.bot.v.play(discord.FFmpegPCMAudio("audio/blyat.ogg"))
-            player.start()
+            vc = await msg.author.voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio("audio/blyat.ogg"))
             await asyncio.sleep(25)
-            player.stop()
+            vc.stop()
             try:
-                await self.bot.v.disconnect()
+                await vc.disconnect()
             except:
                 print("Hey lotus why don't you eat a fucking dick")
 
     @commands.command(pass_context=True)
     async def comrade(self, ctx):
-        breeki = ctx.message.clean_content.split(" ")[1:]
-        vodka = []
         cyka = False
 
-        comrades = {"I": "We", "i": "we", "I'm": "We are", "i'm": "we are", "I'll": "We will", "i'll": "we will",
-                    "I'd": "We'd", "i'd": "we'd", "I've": "We have", "i've": "we have", "my": "our", "mine": "ours",
-                    "My": "Our", "Mine": "Ours", "am": "are", "Am": "Are", "Me": "Us", "me": "us"}
-        for cheeki in breeki:
-            if cheeki in comrades.keys():
-                vodka.append("{}".format(comrades[cheeki]))
+        comrades = {"i": "we",
+                    "me": "us",
+                    "am": "are",
+                    "was": "were",
+                    "'m": " are",
+                    "'ll": " will",
+                    "'d": " would",
+                    "'ve": " have",
+                    "my": "our",
+                    "mine": "ours",
+                    'myself': 'ourselves'}
+        ptn = re.compile("""(i|me|myself|mine|my|am|was)(\\.|\\?|!|,|:|;|"|-|'m|'|\n|\\s)""", flags=re.IGNORECASE)
+        raw = re.split(ptn, ctx.message.clean_content)
+
+        for cheeki, breeki in enumerate(raw):
+            if breeki.casefold() in comrades.keys():
+                if breeki.islower():
+                    raw[cheeki] = comrades[breeki.casefold()].lower()
+                elif breeki.istitle():
+                    raw[cheeki] = comrades[breeki.casefold()].capitalize()
+                elif breeki.isupper():
+                    raw[cheeki] = comrades[breeki.casefold()].upper()
+                else:
+                    raw[cheeki] = comrades[breeki.casefold()]
                 cyka = True
-            else:
-                vodka.append(cheeki)
+
         if cyka:
-            await self.bot.say("*{}\n_**Soviet Anthem Plays**_".format(" ".join(vodka)))
-        else:
-            await self.bot.say(
-                "Remind Evan to come up with a list of funny, ~~racist~~ Soviet quips to replace this message.")
+            await ctx.send("*" + " ".join(raw) + "\n*Soviet Anthem Plays*")
 
     @commands.command()
     async def russkipride(self, ctx):
-        # if self.bot.cooldown_blyat + 360 < datetime.datetime.now().timestamp():
-        #     pass
-        # else:
-        #     await self.bot.say(":musical_note: Guess who's going to the GUUUULAG! The GUUULAG! The GUUUUUULAG! :musical_note:")
-        #     return
+        if self.bot.cooldown_blyat + 360 < datetime.datetime.now().timestamp():
+            pass
+        else:
+            # await ctx.send(":musical_note: Guess who's going to the GUUUULAG! The GUUULAG! The GUUUUUULAG! :musical_note:")
+            return
         try:
             print(dir(ctx))
             print(dir(ctx.author))
             print(dir(ctx.author.voice))
             if ctx.author.voice.channel and not ctx.author.voice.afk:
-                print('a')
                 self.bot.cooldown_blyat = datetime.datetime.now().timestamp()
-                print('a')
-                self.bot.v = await ctx.author.voice.channel.connect()
-                print('a')
-                player = self.bot.v.play(discord.FFmpegPCMAudio("audio/blyat.ogg"))
-                print('a')
+                vc = await ctx.author.voice.channel.connect()
+                vc.play(discord.FFmpegPCMAudio("audio/blyat.ogg"))
                 await asyncio.sleep(25)
-                print('a')
-                player.stop()
-                print('a')
-                await self.bot.v.disconnect()
-                print('a')
-        except Exception as e:
-            print("Hey lotus why don't you eat a fucking dick")
-            print(e)
-            # await ctx.send("CYKA GO TO GULAG BLYAT. DROP AYY DOOBOOYUH PEE RASH B")
-            raise
+                vc.stop()
+                await vc.disconnect()
         except:
-            print("FUCK")
-        finally:
-            for x in self.bot.voice_clients:
-                await x.disconnect()
+            print("Hey lotus why don't you eat a fucking dick")
+
 
 def setup(bot):
     bot.add_cog(Comrade(bot))
