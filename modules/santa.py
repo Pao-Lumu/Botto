1d76faa355873f4265e5da1452529ad66af4dd6f
Evan Johnston <evan@pfg.dom>
2019-10-15 18:03:15 -0500

    Santa stuff

--- modules/santa.py
+++ modules/santa.py
@@ -7,6 +7,8 @@ import sqlite3
 import discord
 from discord.ext import commands
 
+from utils import helpers
+
 
 class Santa(commands.Cog):
     """Ho ho ho you're a ho."""
@@ -14,6 +16,16 @@ class Santa(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.hohoholy_blessings = asyncio.Lock()
+        # self.santa_channel = self.bot.get_channel('')
+
+    @helpers.is_human()
+    @commands.Cog.listener()
+    async def on_add_reaction(self, reaction, author):
+        await self.bot.wait_until_ready()
+        # if message.channel.id == self.santa_channel.id and reaction.emoji == '\N{OK HAND SIGN}':
+        #     if message.clean_content[0] != '#' and message.clean_content[0] != '>':
+        #         await self.auto_comrade_check(message)
+        # await self.auto_thonk(message)
 
     @commands.command()
     async def secret(self, ctx):
@@ -42,7 +54,7 @@ class Santa(commands.Cog):
                     random.shuffle(people)
                     for x in range(len(people)):
                         g, r = x % len(people), (x + 1) % len(people)
-                        if 'evan' in (people[g], people[r]) and 'zach' in (people[g], people[r]):
+                        if 'Evan' in (people[g], people[r]) and 'Zach' in (people[g], people[r]):
                             break
                     else:
                         break
@@ -61,14 +73,18 @@ class Santa(commands.Cog):
                     gifter = person.capitalize()
                     giftee = people[(x + 1) % len(people)].capitalize()
                     message = """
-{}, you have been assigned {} as your secret santa.
-If you'd like to ask them their shirt size, shoe size, favorite color, ideal date location, a/s/l (okay maybe not those last two...) use `>ask`
+{}, you have been assigned {}'s secret santa.
+If you'd like to ask them their shirt size, shoe size, favorite color, ideal date location, a/s/l (okay maybe not those last two...) use `>ask` 
+To ask everyone in the group the above, use `>askall`
 If your asks you a question, and you want to respond, use `>respond`
+It's recommended that you go invisible on Discord when you send `>ask` questions, since I can't prevent people from puzzling that out.
 
 Recommended price limit: ~$30
+Secret Santa gifts can be silly or serious.
+Example: For Xmas 2018, Brandon got an autism shirt, Zach got a unicorn plush, and Aero got a DIY shelf for his stuff.
 
 Please try not to give away who you are to your secret santa, as that ruins the fun of the event.
-Also, misleading your secret santa into thinking they're getting one gift and giving them a different one is allowed and encouraged.
+Misleading your secret santa and giving them a different one is allowed & encouraged.
 """.format(gifter, giftee)
                     # member = self.bot.get_user(141752316188426241)
                     member = self.bot.get_user(discord_id)
@@ -90,8 +106,7 @@ Also, misleading your secret santa into thinking they're getting one gift and gi
             u_id, u_name, _, _, g_id, g_name = cursor.fetchone()
 
             member = self.bot.get_user(int(g_id))
-            msg = "From your gifter:\n {}".format(ctx.message.clean_content.lstrip(
-                str(ctx.prefix) + str(ctx.command)))
+            msg = "From your gifter:\n {}".format(ctx.message.clean_content.lstrip(str(ctx.prefix) + str(ctx.command)))
 
             await member.send(content=msg)
 
@@ -111,33 +126,44 @@ Also, misleading your secret santa into thinking they're getting one gift and gi
     @commands.command(aliases=['question', 'poll'])
     async def anonquestion(self, ctx):
         await ctx.send(
-            'Now creating an anonymized question.\nA preview will be displayed in the below message.')
-        e = discord.Embed(title='Placeholder')
-        preview = await self.send_with_reactions(ctx, embed=e)
-
-        await self.wait_for_reaction(ctx, await self.send_with_reactions(ctx, 'Would you like to add options?'))
-
-        number = 0
+            'Now creating an anonymized question.\nPlease try to keep a professional tone.\nRespond with your question.')
         while True:
-            number += 1
-            await ctx.send(f'Option {number}:')
-            val = await self._get_next_message(ctx)
-            e.add_field(name=f'Option {number}', value=val.clean_content)
-            await preview.edit(embed=e)
-            await self.wait_for_reaction(ctx,
-                                         await self.send_with_reactions(ctx, 'Would you like to add another option?'))
+            e = discord.Embed(color=discord.Color.green())
+            e.set_author(name="Someone asked...")
+            try:
+                val = (await self._get_next_message(ctx)).clean_content.lstrip(str(ctx.prefix) + str(ctx.command))
+            except TimeoutError:
+                await ctx.send('Timed out. Please send the `>anonquestion` command again to write a question.')
+                break
+            e.description = val
+            preview = await self.send_with_yes_no_reactions(ctx,
+                                                            message='This is how your question will look. Are you sure you want to send this message?',
+                                                            embed=e)
+            try:
+                reaction = await self.get_yes_no_reaction(preview)
+                if reaction:
+                    await ctx.send('Okay, sending...')
+                    # SEND TO QUESTION CHANNEL
+                    await ctx.send(embed=e)
+                    break
+                else:
+                    await ctx.send('Okay, please type your question again.')
+            except asyncio.TimeoutError:
+                await ctx.send('Timed out. Please send the `>anonquestion` command again to write a question.')
+                break
+            except asyncio.CancelledError:
+                await ctx.send('Okay, canceled question creation.')
+                break
 
-    async def _get_next_message(self, ctx):
-        try:
-            msg = await self.bot.wait_for('message', timeout=120.0,
-                                          check=lambda message: message.channel == ctx.channel and isinstance(
-                                              ctx.channel, discord.DMChannel))
-        except Exception as e:
-            print('{}: {}'.format(type(e).__name__, e))
+    async def _get_next_message(self, ctx) -> discord.Message:
+        msg = await self.bot.wait_for('message', timeout=120.0,
+                                      check=lambda message: message.channel == ctx.channel and isinstance(
+                                          ctx.channel, discord.DMChannel))
         return msg
 
-    async def send_with_reactions(self, ctx, message: str = '', embed: discord.Embed = None,
-                                  reactions=('\N{THUMBS UP}', '\N{THUMBS DOWN}', '\N{CROSS MARK}')):
+    async def send_with_yes_no_reactions(self, ctx, message: str = '', embed: discord.Embed = None):
+        reactions = ('\N{THUMBS UP SIGN}', '\N{THUMBS DOWN SIGN}', '\N{CROSS MARK}')
+
         if message and embed:
             msg = await ctx.send(message, embed=embed)
         elif embed:
@@ -145,37 +171,42 @@ Also, misleading your secret santa into thinking they're getting one gift and gi
         else:
             msg = await ctx.send(message)
 
-        for reaction in reactions:
-            try:
-                await msg.add_reaction(reaction)
-            except Exception as e:
-                print('{}: {}'.format(type(e).__name__, e))
-                # TODO IMPLEMENT THIS
-                await self.bot.bprint('{}: {}'.format(type(e).__name__, e))
-                # await self.bot('{}: {}'.format(type(e).__name__, e))
-                await ctx.send('Something went wrong. Evan has been notified. (Probably)')
-                break
+        try:
+            await asyncio.gather(*[msg.add_reaction(reaction) for reaction in reactions])
+        except Exception as e:
+            print('{}: {}'.format(type(e).__name__, e))
+            # TODO IMPLEMENT THIS
+            await self.bot.bprint('{}: {}'.format(type(e).__name__, e))
+            # await self.bot('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('Something went wrong. Evan has been notified. (Probably)')
 
         return msg
 
-    async def wait_for_reaction(self, ctx, message: discord.Message, timeout=120.0):
+    async def get_yes_no_reaction(self, ctx, message: discord.Message = None, timeout=120.0):
         def same_channel(reaction, user):
             if message:
                 return ctx.author == user and reaction.message.channel == ctx.channel and reaction.message == message
             else:
                 return ctx.author == user and reaction.message.channel == ctx.channel
 
-        return await self.bot.wait_for('reaction_add', timeout=timeout, check=same_channel)
+        reaction, author = await self.bot.wait_for('reaction_add', timeout=timeout, check=same_channel)
 
-    # @commands.command()
-    # async def get_reaction(self, ctx):
-    #     def check(reaction, user):
-    #         return ctx.author == user
-    #
-    #     pp = await self.bot.wait_for('reaction_add', timeout=120.0, check=check)
-    #     print('{}: {}'.format(type(pp).__name__, pp))
-    #     await ctx.send('{}: {}'.format(type(pp).__name__, pp))
-    #
+        if reaction.emoji == '\N{THUMBS UP SIGN}':
+            return True
+        elif reaction.emoji == '\N{THUMBS DOWN SIGN}':
+            return False
+        elif reaction.emoji == '\N{CROSS MARK}':
+            raise asyncio.CancelledError
+
+    @commands.command()
+    async def get_reaction(self, ctx):
+        def check(_, user):
+            return ctx.author == user
+
+        pp = await self.bot.wait_for('reaction_add', timeout=120.0, check=check)
+        print('{}: {} --- {}'.format(type(pp).__name__, pp, pp[0].emoji))
+        await ctx.send('{}: {}'.format(type(pp).__name__, pp))
+
     # @commands.command()
     # async def r(self, ctx):
     #     print(ctx.message.clean_content.lstrip(str(ctx.prefix) + str(ctx.command)))
