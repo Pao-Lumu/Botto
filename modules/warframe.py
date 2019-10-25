501b6cf7f7a84ac2c2b667776865e0907053dc94
Evan Johnston <evan@pfg.dom>
2019-10-25 16:17:45 -0500

    Remove bad code from a bad decision

--- modules/warframe.py
+++ modules/warframe.py
@@ -62,25 +62,12 @@ class Warframe(commands.Cog):
             msg = await ctx.send(embed=e)
 
             for offer in info['inventory']:
-                if "Primed" in offer['item'] or "Wraith" in offer['item'] or "Prisma" in offer['item']:
-                    name = str(offer['item']).lower().replace(' ', '_')
-                    try:
-                        resp = await self.get_item_statistics(name)
-                        fds = list(map(lambda x: x['wa_price'], resp['payload']['90days']))
-                        minimum = resp['payload']['90days']['min_price']
-                        maximum = resp['payload']['90days']['max_price']
-                        wa_over_90 = sum(fds) / len(fds)
-                        e.add_field(name=offer['item'],
-                                    value=f"{offer['ducats']} ducats + {offer['credits']} credits ({minimum}|{wa_over_90}|{maximum} platinum)")
-                    except TypeError:
-                        e.add_field(name=offer['item'], value=f"{offer['ducats']} ducats + {offer['credits']} credits")
-                else:
-                    e.add_field(name=offer['item'], value=f"{offer['ducats']} ducats + {offer['credits']} credits")
+                e.add_field(name=offer['item'], value=f"{offer['ducats']} ducats + {offer['credits']} credits")
 
             dukey = "{0} is currently at {1}, and will leave on {2} {3}.".format(info['character'], info['location'],
                                                                                  hr_expiry, info['endString'])
 
-            await msg.edit(dukey, embed=e)
+            await msg.edit(content=dukey, embed=e)
 
     @commands.command()
     async def nightwave(self, ctx):
