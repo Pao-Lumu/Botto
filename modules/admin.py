07917a538c3f66d9d8e1ec241608946c85b91bb0
Evan Johnston <evan@example.com>
2020-01-08 01:12:10 -0600

    Kanye.

--- modules/admin.py
+++ modules/admin.py
@@ -1,5 +1,9 @@
 # noinspection PyPackageRequirements
+import discord
+import asyncio
 from discord.ext import commands
+from utils import helpers
+from datetime import datetime
 
 
 class Admin(commands.Cog):
@@ -30,6 +34,28 @@ class Admin(commands.Cog):
     async def echo(self, ctx):
         msg = ctx.message.clean_content.lstrip(str(ctx.prefix) + str(ctx.command))
         await ctx.send(content=msg)
+
+    @helpers.is_human()
+    @commands.command()
+    async def kanye(self, ctx: commands.Context):
+        try:
+            if ctx.author.voice.channel and not ctx.author.voice.afk:
+                # if ctx.author.voice.channel is not self.bot.voice.channel:
+                vc = await ctx.author.voice.channel.connect()
+                if 'cursed' in ctx.message.clean_content:
+                    vc.play(discord.FFmpegPCMAudio(source="audio/kanye_singing_cursed.ogg"))
+                else:
+                    vc.play(discord.FFmpegPCMAudio("audio/kanye_singing.ogg"))
+                await asyncio.sleep(30)
+                vc.stop()
+
+                await vc.disconnect()
+        except AttributeError as e:
+            print(e)
+            await ctx.send("Not in voice chat.")
+        except OSError as e:
+            print(e)
+            self.bot.bprint("Hey lotus why don't you eat a fucking dick ~ Zach 2018")
 
 
 def setup(bot):
