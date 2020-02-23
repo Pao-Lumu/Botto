dfc02f822f619d654d1ef1454660e7d88388090d
Evan Johnston <evan@example.com>
2020-02-23 01:50:38 -0600

    Fix error caused by superfluous enum

--- aliases/warframe.py
+++ aliases/warframe.py
@@ -1,7 +1,4 @@
-import enum
-
-
-class Warframe(enum.Enum):
+class Warframe:
     BARO = ['barowhen', 'dukey', 'vt', 'whenthefuckdoesbarokattiercomeyoufuck', 'ducats', 'voidtrader', 'barokiteer',
             'baroken', 'professionalstreetnigga', 'barrow', 'guywithbinhisname', 'bman', 'baraocattier', "BaroKi'teer",
             'fuckyouzach', 'kyslmao', 'barrowcatears', 'barokecatears', 'gimmeprimemods', 'monopolymoney']
